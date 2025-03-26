90786e01191882435c37ca4b55ecf11d622a7c3e
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2025-03-26 21:27:18 +0000

    style: update NavigationBar layout and refine CSS imports and utilities

--- src/components/NavigationBar.ts
+++ src/components/NavigationBar.ts
@@ -10,10 +10,10 @@ export class NavBar extends BaseComponent {
   render() {
     return html`
       <div class="border-b mb-6">
-        <div class="flex gap-6 container py-6">
+        <div class="flex items-center gap-8 container py-6">
           <h3 class="font-bold select-none">ft_transcendance</h3>
           <div
-            class="flex gap-4 text-sm [&>a]:transition-colors [&>a]:text-muted-foreground [&>a]:hover:text-foreground"
+            class="flex items-center gap-6 [&>a]:transition-colors [&>a]:text-muted-foreground [&>a]:hover:text-foreground"
           >
             <a href="/">Home</a>
             <a href="/leaderboard">Leaderboard</a>
